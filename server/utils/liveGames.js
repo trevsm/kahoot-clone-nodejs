42df349a48f5cd51a4561ebf18ed9bda1d718b57
Trevor <trevorsm@pm.me>
2022-02-26 23:31:16 -0700

    formatting

--- server/utils/liveGames.js
+++ server/utils/liveGames.js
@@ -1,23 +1,23 @@
 class LiveGames {
-    constructor () {
-        this.games = [];
+  constructor() {
+    this.games = [];
+  }
+  addGame(pin, hostId, gameLive, gameData) {
+    var game = { pin, hostId, gameLive, gameData };
+    this.games.push(game);
+    return game;
+  }
+  removeGame(hostId) {
+    var game = this.getGame(hostId);
+
+    if (game) {
+      this.games = this.games.filter((game) => game.hostId !== hostId);
     }
-    addGame(pin, hostId, gameLive, gameData){
-        var game = {pin, hostId, gameLive, gameData};
-        this.games.push(game);
-        return game;
-    }
-    removeGame(hostId){
-        var game = this.getGame(hostId);
-        
-        if(game){
-            this.games = this.games.filter((game) => game.hostId !== hostId);
-        }
-        return game;
-    }
-    getGame(hostId){
-        return this.games.filter((game) => game.hostId === hostId)[0]
-    }
+    return game;
+  }
+  getGame(hostId) {
+    return this.games.filter((game) => game.hostId === hostId)[0];
+  }
 }
 
-module.exports = {LiveGames};
+module.exports = { LiveGames };
